091e29c63513f8f239260e4e66a2bffe984b2380
Prashant Kulkarni <prashantkul366@gmail.com>
2026-02-25 00:18:27 +0530

    BUSI

--- train_model.py
+++ train_model.py
@@ -77,8 +77,8 @@ def main_loop(batch_size=config.batch_size, model_type='', tensorboard=True):
         print("Loading MoNuSeg dataset OR BUSI")
         # train_text = read_text(config.train_dataset + 'Train_text.xlsx')
         # val_text = read_text(config.val_dataset + 'Val_text.xlsx')
-        train_text = read_text(config.train_dataset + 'Train_text.xlsx')
-        val_text   = read_text(config.val_dataset + 'Test_text.xlsx')
+        train_text = read_text(config.train_dataset + '/Train_text.xlsx')
+        val_text   = read_text(config.val_dataset + '/Test_text.xlsx')
         train_dataset = ImageToImage2D(config.train_dataset, config.task_name, train_text, train_tf,
                                        image_size=config.img_size)
         val_dataset = ImageToImage2D(config.val_dataset, config.task_name, val_text, val_tf, image_size=config.img_size)
